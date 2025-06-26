4794b1b3e5c2e878d48720470df8a10115db26d7
Halsey26 <omara.cassano@gmail.com>
2025-06-26 16:49:14 -0500

    api

---
+++ README.md
@@ -0,0 +1,45 @@
+# FastAPI Chatbot con Supabase 
+
+Esta API recibe logs desde un [chatbot](https://github.com/Halsey26/chat_demo_ecommerce) y los envía a una base de datos Supabase.
+
+## Instalación local
+
+```bash
+git clone https://github.com/tuusuario/chatbot-api.git
+cd chatbot-api
+pip install -r requirements.txt
+```
+
+
+---
+## Para trabajar con supabase
+
+### Librerias necesarias
+En el entorno virtual
+```
+pip install pydantic<2 requests python-dotenv
+```
+
+---
+### Configuración adicional
+
+1. Crear el archivo .env 
+En la raíz del proyecto, crea un archivo llamado `.env` y coloca dentro tu clave de API de Supabase:
+
+```
+SUPABASE_URL=sk-...
+SUPABASE_SERVICE_ROLE=
+```
+⚠️ Nunca compartas públicamente tu clave de API.
+
+---
+## NOTAS:
+En supabase: En un proyecto debes crear una tabla con la misma estructura:
+- id_conversacion: text
+- id_usuario: text
+- rol: text
+- mensaje: text
+- fecha: timestamp
+
+Luego de crear una tabla, ejemplo: `logs_chat`.
+Obtener las api key y almacenarla en .env
---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv() #carga el archivo .env
+SUPABASE_URL= os.getenv("SUPABASE_URL")
+SUPABASE_KEY= os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+
+# prueba de que se cargan correctamente las variables de entorno
+# print(SUPABASE_URL)
+# print(SUPABASE_KEY)
+# print('prueba variables')
+
+app = FastAPI()
+app.title = 'API CHATBOT - FASTAPI - SUPABASE'
+
+class logs_chat(BaseModel):
+    id_conversacion: str
+    id_usuario: str
+    rol: str
+    mensaje: str
+    fecha: datetime
+
+@app.post("/logs", tags=['Chatbot'])
+def create_log(log:logs_chat):
+    #convertimos a dicc y despues en una lista
+    data = [{
+        **log.dict(),
+        "fecha":log.fecha.isoformat()}] # formato válido para json
+
+    headers = {
+        "apikey" : SUPABASE_KEY, 
+        "Authorization": f"Bearer {SUPABASE_KEY}", 
+        "Content-Type": "application/json",
+        "Prefer": "return=minimal"
+
+    }
+    
+    response = requests.post(f"{SUPABASE_URL}/rest/v1/logs_chat", headers=headers, json=data)
+
+    if response.status_code in [200, 201, 204]:
+        print("✅ Log enviado a Supabase")
+    else:
+        print(f"❌ Error al enviar log: {response.status_code}")
+        print(response.text)
+
+    return {"status": response.status_code}
+
+# documentacion supabase para insert rows
+# curl -X POST 'https://bupbptppncwasereiuea.supabase.co/rest/v1/logs_chat' \
+# -H "apikey: SUPABASE_KEY" \
+# -H "Authorization: Bearer SUPABASE_KEY" \
+# -H "Content-Type: application/json" \
+# -H "Prefer: return=minimal" \
+# -d '{ "some_column": "someValue", "other_column": "otherValue" }'
